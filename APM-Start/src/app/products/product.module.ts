7b01228e205ecf101ab500e3558eaee78277264a
monooi <dracorufus.w@gmail.com>
2021-12-18 23:01:50 +0700

    After remove routing module

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
-import { SharedModule } from '../shared/shared.module';
+
+import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
-import { ProductListComponent } from './product-list.component';
-import { ProductRoutingModule } from './product-routing.module';
+
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     SharedModule,
-    ProductRoutingModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'products/:id/edit', component: ProductEditComponent }
+    ])
   ],
   declarations: [
     ProductListComponent,
